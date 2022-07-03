fcee03d3c29995bb8c41a7d09bd54f4f2874f67b
Fumiya Matsumoto <64899536+Fumiya-Matsumoto@users.noreply.github.com>
2022-07-04 01:31:04 +0900

    test modern javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import "./styles.css";
+const li = ["dog", "cat"];
+console.log(li);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const nameArr = ["田中", "山田", "松本"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index])
+// };
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// })
+
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+//
+
+// 三項演算子
+const val1 = 1 > 0 ? "trueです" : "falseです";
+console.log(val1);
+
+const num = 1300;
+
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+};
+
+console.log(checkSum(50, 20));
